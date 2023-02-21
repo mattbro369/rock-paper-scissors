6a00f51e8eb83556db0164e1cc4b3db0e58f202a
Matt <ethacktry@gmail.com>
2023-02-21 20:17:36 +0100

    Added user prompt, with checks for incorrect input

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const rps = ["Rock", "Paper", "Scissors"];
+// TODO
+// FIX THE CHOICE VALIDATION
+// SET THE GAME TO RESTART
+
+
+const rps = ["rock", "paper", "scissors"];
+const computerSelection = getComputerChoice();
+const computerSelectionFinal = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+const playerSelection = getPlayerChoice();
+const playerSelectionFinal = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 
 
 // Computer chooses
@@ -6,18 +15,28 @@ function getComputerChoice() {
     return rps[Math.floor(Math.random() * rps.length)];
 }
 
+// get and check user input
+function getPlayerChoice() {
+    let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors" || playerChoice === "") {
+        playerChoice = prompt("Please enter: Rock, Paper or Scissors").toLowerCase();
+    }
+    return playerChoice;
+}
+
 // // Function to play round, taking player choice as parameter
 function playRound(playerSelection, computerSelection) {
-    if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
-        alert(`Player wins, ${playerSelection} beats ${computerSelection}`);
+    if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
+        alert(`Player wins, ${playerSelectionFinal} beats ${computerSelectionFinal}`);
     } else if (playerSelection === computerSelection) {
         alert("It's a tie!");
     } else {
-        alert(`Computer wins! ${computerSelection} beats ${playerSelection}`);
+        alert(`Computer wins! ${computerSelectionFinal} beats ${playerSelectionFinal}`);
     }
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = "Paper";
-console.log(playRound(playerSelection,computerSelection));
+
+
+// playRound(playerSelection, computerSelection);
+
 
