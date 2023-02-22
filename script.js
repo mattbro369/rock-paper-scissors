643a2c6e43cd2c87195c76546448ea94a050824d
Matt <ethacktry@gmail.com>
2023-02-22 11:50:41 +0100

    Added replay game 5 rounds, plus score checker

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
 // TODO
-// FIX THE CHOICE VALIDATION
-// SET THE GAME TO RESTART
+// SET THE GAME TO RESTART 5 TIMES
+// PLAYER CHOICE NEEDS TO BE INSIDE THE PLAYROUND - CHOICE MUST BE MADE EVERY ROUND
 
 
 const rps = ["rock", "paper", "scissors"];
-const computerSelection = getComputerChoice();
-const computerSelectionFinal = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-const playerSelection = getPlayerChoice();
-const playerSelectionFinal = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-
+let computerSelection;
+let computerSelectionFinal; 
+let playerSelection;
+let playerSelectionFinal;
+let playerScore = 0;
+let computerScore = 0;
 
 // Computer chooses
 function getComputerChoice() {
-    return rps[Math.floor(Math.random() * rps.length)];
+    computerSelection = rps[Math.floor(Math.random() * rps.length)];
+    return computerSelectionFinal = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
 }
 
 // get and check user input
-function getPlayerChoice() {
-    let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-    while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors" || playerChoice === "") {
-        playerChoice = prompt("Please enter: Rock, Paper or Scissors").toLowerCase();
+function getPlayerChoice(playerSelection) {
+    playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+    while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors" || playerSelection === "") {
+        playerSelection = prompt("Please enter: Rock, Paper or Scissors").toLowerCase();
     }
-    return playerChoice;
+    return playerSelectionFinal = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 }
 
 // // Function to play round, taking player choice as parameter
-function playRound(playerSelection, computerSelection) {
-    if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
-        alert(`Player wins, ${playerSelectionFinal} beats ${computerSelectionFinal}`);
-    } else if (playerSelection === computerSelection) {
-        alert("It's a tie!");
+function playRound(playerSelectionFinal, computerSelectionFinal) {
+    if ((playerSelectionFinal === "Rock" && computerSelectionFinal === "Scissors") || (playerSelectionFinal === "Paper" && computerSelectionFinal === "Rock") || (playerSelectionFinal === "Scissors" && computerSelectionFinal === "Paper")) {
+        console.log(`Player wins, ${playerSelectionFinal} beats ${computerSelectionFinal}`);
+        return playerScore++;
+    } else if (playerSelectionFinal === computerSelectionFinal) {
+        console.log("It's a tie!");
+        return;
     } else {
-        alert(`Computer wins! ${computerSelectionFinal} beats ${playerSelectionFinal}`);
+        console.log(`Computer wins! ${computerSelectionFinal} beats ${playerSelectionFinal}`);
+        return computerScore++;
+    }
+    
+}
+
+// GAME FUNCTION TO REPLAY GAME 5 TIMES
+// GAME MUST KEEP TRACK OF SCORE (CONSOLE.LOG)
+function game(round) {  
+    for (i = 1; i <= 5; i++) {
+    getComputerChoice();
+    getPlayerChoice();
+    playRound(playerSelectionFinal, computerSelectionFinal);
+    console.log(`Player: ${playerScore}`);
+    console.log(`Computer: ${computerScore}`);
     }
 }
 
-
-
-// playRound(playerSelection, computerSelection);
-
-
+game();
