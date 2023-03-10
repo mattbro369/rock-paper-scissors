3d3b104d145029bbf05e8158abd06b583617b908
Matt <ethacktry@gmail.com>
2023-03-10 13:36:16 +0100

    Added event listeners to select choices by clicking

--- script.js
+++ script.js
@@ -1,72 +1,64 @@
 const rps = ["rock", "paper", "scissors"];
 let computerSelection;
-let computerSelectionFinal; 
 let playerSelection;
-let playerSelectionFinal;
 let playerScore = 0;
 let computerScore = 0;
 let winner = false;
 
-// Computer chooses
+const btn = document.querySelectorAll(".buttons-game");
+btn.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    playerSelection = e.target.id;
+    game();
+  });
+  return playerSelection;
+});
+
+function game() {
+  if (winner === false) {
+    getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    printScore();
+    checkScore(playerScore, computerScore);
+  }
+}
+
 function getComputerChoice() {
-    computerSelection = rps[Math.floor(Math.random() * rps.length)];
-    return computerSelectionFinal = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+  computerSelection = rps[Math.floor(Math.random() * rps.length)];
+  return computerSelection;
 }
 
-// get and check user input
-function getPlayerChoice(playerSelection) {
-    playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors" || playerSelection === "") {
-        playerSelection = prompt("Please enter: Rock, Paper or Scissors").toLowerCase();
-    }
-    return playerSelectionFinal = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    console.log(`Player wins, ${playerSelection} beats ${computerSelection}`);
+    return playerScore++;
+  } else if (playerSelection === computerSelection) {
+    console.log("It's a tie!");
+    return;
+  } else {
+    console.log(`Computer wins! ${computerSelection} beats ${playerSelection}`);
+    return computerScore++;
+  }
 }
 
-// // Function to play round, taking player choice as parameter
-function playRound(playerSelectionFinal, computerSelectionFinal) {
-    if ((playerSelectionFinal === "Rock" && computerSelectionFinal === "Scissors") || (playerSelectionFinal === "Paper" && computerSelectionFinal === "Rock") || (playerSelectionFinal === "Scissors" && computerSelectionFinal === "Paper")) {
-        console.log(`Player wins, ${playerSelectionFinal} beats ${computerSelectionFinal}`);
-        return playerScore++;
-    } else if (playerSelectionFinal === computerSelectionFinal) {
-        console.log("It's a tie!");
-        return;
-    } else {
-        console.log(`Computer wins! ${computerSelectionFinal} beats ${playerSelectionFinal}`);
-        return computerScore++;
-    }
-    
-}
-
-// Function to print score
 function printScore() {
-    console.log(`Player: ${playerScore}`);
-    console.log(`Computer: ${computerScore}`);
-    console.log("");
+  console.log(`Player: ${playerScore}`);
+  console.log(`Computer: ${computerScore}`);
+  console.log("");
 }
 
-// Function to check score and print if winner 
 function checkScore(playerScore, computerScore) {
-    if (playerScore === 5) {
-        alert("Player wins!");
-        return winner = true;
-    } else if (computerScore === 5) {
-        alert("Computer wins!")
-        return winner = true;
-    } else {
-        return winner;
-    }
+  if (playerScore === 5) {
+    alert("Player wins!");
+    return (winner = true);
+  } else if (computerScore === 5) {
+    alert("Computer wins!");
+    return (winner = true);
+  } else {
+    return winner;
+  }
 }
-
-// Function to play round until score reaches 5, including all previous declared functions
-function game(round) {
-    do {
-        getComputerChoice();
-        getPlayerChoice();
-        playRound(playerSelectionFinal, computerSelectionFinal);
-        printScore();
-        checkScore(playerScore, computerScore);
-    } while (winner === false)
-}
-
-// Calling game
-// game(playRound);
